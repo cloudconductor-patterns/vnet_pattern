084744b201374a62c947c3f5c65591630f74cefb
Masafumi Okamoto <okamoto.masafumi@tis.co.jp>
2015-08-05 10:34:03 +0900

    Modify metadata.rb into vnet_part cookbook

--- site-cookbooks/vnet_part/metadata.rb
+++ site-cookbooks/vnet_part/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'ccndctr@gmail.com'
 license 'All rights reserved'
 description 'Installs/Configures vnet_part'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.0.1'
+version '0.1.0'
 
 supports 'centos', '= 6.5'
 
