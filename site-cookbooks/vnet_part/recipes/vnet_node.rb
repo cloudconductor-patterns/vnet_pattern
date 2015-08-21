acdbf02bec83c9e77af8ab9a3bda4810e6e13be4
Masafumi Okamoto <okamoto.masafumi@tis.co.jp>
2015-08-21 10:13:18 +0900

    modify to append the guard on the vnet_node recipe so as not run on the vnet-edge server

--- site-cookbooks/vnet_part/recipes/vnet_node.rb
+++ site-cookbooks/vnet_part/recipes/vnet_node.rb
@@ -20,27 +20,31 @@ def edge_server
   servers.first
 end
 
-gretap_interfaces(host_info).each do |ifname, ifcfg|
-  host_name = host_info['hostname']
-  remote_addr = edge_server['private_ip']
-  local_addr = host_info['private_ip']
-  virtual_addr = ifcfg['virtual_address']
-  virtual_prefix = ifcfg['virtual_prefix']
-  virtual_prefix ||= virtual_network_prefix(ifcfg['network'])
+def configure_interfaces
+  gretap_interfaces(host_info).each do |ifname, ifcfg|
+    host_name = host_info['hostname']
+    remote_addr = edge_server['private_ip']
+    local_addr = host_info['private_ip']
+    virtual_addr = ifcfg['virtual_address']
+    virtual_prefix = ifcfg['virtual_prefix']
+    virtual_prefix ||= virtual_network_prefix(ifcfg['network'])
 
-  vnet_part_gretap ifname do
-    remote_addr remote_addr
-    local_addr local_addr
-    virtual_addr virtual_addr
-    virtual_prefix virtual_prefix
-  end
+    vnet_part_gretap ifname do
+      remote_addr remote_addr
+      local_addr local_addr
+      virtual_addr virtual_addr
+      virtual_prefix virtual_prefix
+    end
 
-  cloudconductor_server_interface "#{host_name}_#{ifname}" do
-    hostname host_name
-    if_name ifname
-    remote_address remote_addr
-    local_address local_addr
-    virtual_address virtual_addr
-    virtual_prefix virtual_prefix
+    cloudconductor_server_interface "#{host_name}_#{ifname}" do
+      hostname host_name
+      if_name ifname
+      remote_address remote_addr
+      local_address local_addr
+      virtual_address virtual_addr
+      virtual_prefix virtual_prefix
+    end
   end
 end
+
+configure_interfaces unless host_info['roles'].include?('vna') || host_info['roles'].include?('vnmgr')
