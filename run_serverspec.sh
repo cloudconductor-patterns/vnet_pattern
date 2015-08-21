05192da83f5807eee2bc7c00b0563b136a391f1b
Masafumi Okamoto <okamoto.masafumi@tis.co.jp>
2015-08-21 13:40:33 +0900

    Modify to log output from the run_serverspec.sh

--- run_serverspec.sh
+++ run_serverspec.sh
@@ -5,6 +5,12 @@ script_root=$(cd $(dirname $0) && pwd)
 pattern_root=${script_root}
 test_root=${pattern_root}/serverspec
 spec_dir=${test_root}/spec
+log_dir=${pattern_root}/logs
+LOG_FILE=${log_dir}/event-handler.log
+
+if [ ! -d ${log_dir} ]; then
+  mkdir ${log_dir}
+fi
 
 if [ "${CONSUL_SECRET_KEY}" == "" ]; then
   CONSUL_SECRET_KEY=$(cat /etc/consul.d/default.json | jq -r .acl_master_token)
@@ -35,17 +41,43 @@ run() {
   [ -z "$T" ] || set -T
 }
 
+function log() {
+  level="$1"
+  message="$2"
+  echo "[`date +'%Y-%m-%dT%H:%M:%S'`] ${level}: ${message}" >> ${LOG_FILE}
+}
+
+log_info() {
+  message="$1"
+  log "INFO" "${message}"
+}
+
+log_error() {
+  message="$1"
+  log "ERROR" "${message}"
+}
+
 execute_serverspec() {
   local roles=($(echo $1 | tr -s ',' ' '))
   local event=$2
 
   for role in ${roles[@]}; do
-    if [ -f "${spec_dir}/${role}/${role}_${event}_spec.rb" ]; then
+    spec_file=${spec_dir}/${role}/${role}_${event}_spec.rb
+    if [ -f "${spec_file}" ]; then
+      log_info "execute serverspec with [${spec_file}]"
+
       run sh -c "cd ${test_root}; rake spec['${role}','${event}']"
       if [ ${status} -ne 0 ]; then
+        log_error 'finished abnormally.'
+        log_error "${output}"
+
         echo "${output}" >&2
         return $status
+      else
+        log_info 'finished successfully.'
       fi
+    else
+      log_info "spec file [${spec_file}] does not exist. skipped."
     fi
   done
 }
